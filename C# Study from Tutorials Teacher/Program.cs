8505ca54339a452fe0354eec4bbf6f85e2af9f2b
Leah Kirk <leah.kirks@gmail.com>
2023-08-09 12:46:16 -0230

    Fixed the format. My problem was calling Main while working in .Net7

--- C# Study from Tutorials Teacher/Program.cs
+++ C# Study from Tutorials Teacher/Program.cs
@@ -1,21 +1,30 @@
 ﻿// Passing Value Type data variables
 
+
+
+int i = 100;
+
+
+Console.WriteLine(i);
+ChangeValue(i);
+Console.WriteLine(i);
+
+//output should be:
+Console.WriteLine("100 \n200\n100");
+
+
+
+
+
+
+
+
+
+
+
 static void ChangeValue(int x)
 {
-    x =  200;
+    x = 200;
 
     Console.WriteLine(x);
 }
-
-static void Main(string[] args)
-{
-    int i = 100;
-
-    Console.WriteLine(i);
-    
-    ChangeValue(i);
-    
-    Console.WriteLine(i);
-}
-//output should be:
-Console.WriteLine("100 \n200\n100");
