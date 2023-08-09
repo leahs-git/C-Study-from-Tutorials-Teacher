67df308291aea685a77f714c8a2a84df575e075c
Leah Kirk <leah.kirks@gmail.com>
2023-08-09 11:30:34 -0230

    Add project files.

---
+++ C# Study from Tutorials Teacher/Program.cs
@@ -0,0 +1,19 @@
+﻿// Passing Value Type data variables
+
+static void ChangeValue(int x)
+{
+    x =  200;
+
+    Console.WriteLine(x);
+}
+
+static void Main(string[] args)
+{
+    int i = 100;
+
+    Console.WriteLine(i);
+    
+    ChangeValue(i);
+    
+    Console.WriteLine(i);
+}
